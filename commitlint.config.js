f234a8063ec7bfae777a90dcdec0046d73407d81
solarlime <solarlime@outlook.com>
2026-03-24 20:49:47 +0300

    feat: initial commit

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // New feature
+        'fix', // Bug fix
+        'docs', // Documentation changes
+        'style', // Code style changes (formatting, etc)
+        'refactor', // Code refactoring
+        'perf', // Performance improvements
+        'test', // Test changes
+        'chore', // Build process, dependency updates
+        'ci', // CI configuration changes
+        'build', // Build system changes
+        'revert', // Revert previous commit
+      ],
+    ],
+    'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
+    'subject-max-length': [2, 'always', 72],
+    'body-max-line-length': [2, 'always', 100],
+  },
+};
